5e4835a1d78b0556a9a45954d7e6e7719b723194
sabbir <sabbirrahman42@iut-dhaka.edu>
2021-10-16 20:10:55 +0600

    reset to back

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.get('/', (req,res) => {
 })
 
 const PORT = process.env.PORT 
-const URL = 'mongodb://mongo:27017/quote'
+const URL = 'mongodb+srv://sabbir:sabbir@cluster0.mmfp5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
